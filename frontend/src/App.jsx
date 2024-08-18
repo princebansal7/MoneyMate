ab9626ab78b58a722e6f81b8e7c631054b77fc86
Prince Bansal <prince.bansal008@gmail.com>
2024-08-18 06:19:44 +0530

    frontend routes initialised

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { TransferMoney } from "./pages/TransferMoney";
 
 function App() {
-    console.log("hello");
-    return <>Hello There</>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/transfer" element={<TransferMoney />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
